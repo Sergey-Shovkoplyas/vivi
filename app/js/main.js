957d2336d646ca44e588de14a44b0504f49ae7ea
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-03-31 20:03:23 +0300

    Add colection section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 $( document ).ready(function() {
 
-	console.log( 'JQuery!!!s' );
+	// -------------------- initialize sliders ---------------------
+
+	$('.home__collections-slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		variableWidth: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					variableWidth: false,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false
+				}
+			},
+			
+		]
+	});
+	console.log( 'JQuery!!!' );
 
 });
