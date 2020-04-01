b9004d238023e0946c285d2d767a2789889932e1
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-04-02 00:16:10 +0300

    Add sections to home-page

--- app/js/main.js
+++ app/js/main.js
@@ -15,13 +15,48 @@ $( document ).ready(function() {
 				settings: {
 					variableWidth: false,
 					slidesToShow: 1,
-					slidesToScroll: 1,
-					arrows: false
+					slidesToScroll: 1
 				}
 			},
 			
 		]
 	});
+
+	$('.home__look-slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+			
+		]
+	});
+
+	$('.home__popular-slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+			
+		]
+	});
+
+
 	console.log( 'JQuery!!!' );
 
 });
